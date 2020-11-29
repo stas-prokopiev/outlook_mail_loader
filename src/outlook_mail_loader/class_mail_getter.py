4bfef38ac96672413a9a86590836f59b9af3c507
stanislav <stas.prokopiev@gmail.com>
2020-11-29 22:21:46 +0300

    prepearing class_mail_getter to serious work

--- src/outlook_mail_loader/class_mail_getter.py
+++ src/outlook_mail_loader/class_mail_getter.py
@@ -1,4 +1,41 @@
-# TBD
+"""
+Module with class to process dumped mails
+"""
+# Standard library imports
+import os
+import logging
+
+# Third party imports
+from char import char
+from . import mail_listener
+
+# Local imports
+
+LOGGER = logging.getLogger("outlook_mail_loader")
+
 
 class DumpedMails(object):
     pass
+
+    @char
+    def __init__(
+            self,
+            str_path_dir_with_mails="mails",
+    ):
+        """Init object
+
+        Args:
+            str_path_dir_with_mails (str, optional): Dir. from where to load
+        """
+        pass
+
+    @char
+    def get_last_n_letters(self, int_letters_to_get):
+        """"""
+        pass
+
+    def print_stats_about_dumped_mails(self):
+        """"""
+        list_datetimes = []
+        
+        mail_listener.print_stats_about_dumped_mails(list_datetimes)
